bd4eb51ce68c209fd1c7f81b6cae139c3e7ec81e
Yacine Rezgui <yrezgui@google.com>
2020-01-30 17:55:20 +0000

    All Files Access code sample
    Add files generated by Android Studio
    Add copyright header
    
    Change-Id: Ia51b96a5f8d1d45de3afc109884e030a5ae28f23

---
+++ AllFilesAccess/app/src/main/java/com/android/samples/allfilesaccess/MainActivity.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ */
+
+package com.android.samples.allfilesaccess
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.samples.allfilesaccess.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
