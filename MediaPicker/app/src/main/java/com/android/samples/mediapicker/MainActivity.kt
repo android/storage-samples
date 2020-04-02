cfb74b557838747afd56afa5a9346f8df7ca3795
Yacine Rezgui <yrezgui@google.com>
2020-04-02 20:09:15 +0100

    Add media picker
    Change-Id: I66cea818d4c7cf145d16ddb657502fddc43fd399

--- MediaPicker/app/src/main/java/com/android/samples/mediapicker/MainActivity.kt
+++ MediaPicker/app/src/main/java/com/android/samples/mediapicker/MainActivity.kt
@@ -18,13 +18,65 @@
 
 package com.android.samples.mediapicker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import com.android.samples.mediapicker.databinding.ActivityMainBinding
+
+const val IMAGE_MIME_TYPE = "image/*"
+const val VIDEO_MIME_TYPE = "video/*"
+const val AUDIO_MIME_TYPE = "audio/*"
+
+const val FILE_PICKER_REQUEST_CODE = 1
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pickFileButton.setOnClickListener {
+            showMenu(binding.pickFileButton)
+        }
+    }
+
+    private fun showMenu(anchor: View) {
+        val popup = PopupMenu(this, anchor)
+        popup.menuInflater.inflate(R.menu.media_types_menu, popup.menu)
+
+        popup.setOnMenuItemClickListener {
+            pickFile(it.itemId)
+            true
+        }
+
+        popup.show()
+    }
+
+    private fun pickFile(@IdRes type: Int) {
+        val mimeType = when (type) {
+            R.id.imageType -> IMAGE_MIME_TYPE
+            R.id.videoType -> VIDEO_MIME_TYPE
+            R.id.audioType -> AUDIO_MIME_TYPE
+            else -> IMAGE_MIME_TYPE
+        }
+
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = mimeType
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+
+        startActivityForResult(intent, FILE_PICKER_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            println("GOTCHA")
+        }
     }
 }
