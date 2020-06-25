17c3c7fc2147b6a426d366f638a743532c06f3e8
cketti <ck@cketti.de>
2020-06-25 15:35:20 +0200

    Remove flags from ACTION_OPEN_DOCUMENT_TREE Intent
    They are not supported by the ACTION_OPEN_DOCUMENT_TREE Intent. The intended behavior is how ACTION_OPEN_DOCUMENT_TREE works by default.

--- ActionOpenDocumentTree/app/src/main/java/com/example/android/ktfiles/MainActivity.kt
+++ ActionOpenDocumentTree/app/src/main/java/com/example/android/ktfiles/MainActivity.kt
@@ -81,10 +81,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openDirectory() {
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
-            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or
-                    Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
-        }
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
         startActivityForResult(intent, OPEN_DIRECTORY_REQUEST_CODE)
     }
 }
