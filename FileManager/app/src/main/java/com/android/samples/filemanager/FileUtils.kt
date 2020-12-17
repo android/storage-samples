a0723ad17b5b5c1549995bf605c3853462e5a5bd
Mirwise Khan <mirwise001@gmail.com>
2020-12-17 14:23:16 +0500

    Fixed openFile trying to open null Uri

--- FileManager/app/src/main/java/com/android/samples/filemanager/FileUtils.kt
+++ FileManager/app/src/main/java/com/android/samples/filemanager/FileUtils.kt
@@ -19,9 +19,13 @@ package com.android.samples.filemanager
 import android.content.Intent
 import android.net.Uri
 import android.os.Environment
+import android.util.Log
 import android.webkit.MimeTypeMap
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import java.io.File
+
+private const val AUTHORITY = "${BuildConfig.APPLICATION_ID}.provider"
 
 fun getMimeType(url: String): String {
     val ext = MimeTypeMap.getFileExtensionFromUrl(url)
@@ -50,15 +54,15 @@ fun renderItem(activity: AppCompatActivity, file: File): String {
     }
 }
 
+
 fun openFile(activity: AppCompatActivity, selectedItem: File) {
     // Get URI and MIME type of file
-    val uri = Uri.fromFile(selectedItem).normalizeScheme()
+    val uri = FileProvider.getUriForFile(activity.applicationContext, AUTHORITY, selectedItem)
     val mime: String = getMimeType(uri.toString())
 
     // Open file with user selected app
-    val intent = Intent()
-    intent.action = Intent.ACTION_VIEW
-    intent.data = uri
-    intent.type = mime
+    val intent = Intent(Intent.ACTION_VIEW)
+    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    intent.setDataAndType(uri, mime)
     return activity.startActivity(intent)
 }
