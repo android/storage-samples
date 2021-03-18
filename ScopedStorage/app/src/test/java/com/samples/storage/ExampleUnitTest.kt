5b951d1b52af9eaa72e611ced9463fc02ef7577d
Yacine Rezgui <rezgui.y@gmail.com>
2021-03-18 20:26:42 +0000

    Simplify imports

--- ScopedStorage/app/src/test/java/com/samples/storage/ExampleUnitTest.kt
+++ ScopedStorage/app/src/test/java/com/samples/storage/ExampleUnitTest.kt
@@ -16,9 +16,9 @@
 
 package com.samples.storage
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
