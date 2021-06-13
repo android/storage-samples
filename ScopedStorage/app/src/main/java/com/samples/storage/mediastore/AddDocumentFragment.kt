b2e8c5ae1464ffd9c7f184489cca6b521c1379db
Yacine Rezgui <rezgui.y@gmail.com>
2021-06-13 16:53:57 +0100

    Apply spotless

--- ScopedStorage/app/src/main/java/com/samples/storage/mediastore/AddDocumentFragment.kt
+++ ScopedStorage/app/src/main/java/com/samples/storage/mediastore/AddDocumentFragment.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -33,8 +33,7 @@ class AddDocumentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDemoBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 
     override fun onDestroyView() {
--- ScopedStorage/app/src/main/java/com/samples/storage/mediastore/MediaStoreFragment.kt
+++ ScopedStorage/app/src/main/java/com/samples/storage/mediastore/MediaStoreFragment.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,10 +21,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.samples.storage.R
-import com.samples.storage.databinding.FragmentListBinding
 import com.samples.storage.Action
 import com.samples.storage.ActionListAdapter
+import com.samples.storage.R
+import com.samples.storage.databinding.FragmentListBinding
 
 private val demoList = arrayOf(
     Action(R.string.mediastore_add, R.id.action_mediaStoreFragment_to_addMediaFragment),
