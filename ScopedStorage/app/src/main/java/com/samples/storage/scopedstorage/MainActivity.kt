1c7ccf2f4a458f8f93da7f2fb56e49d37ba25f0f
Yacine Rezgui <rezgui.y@gmail.com>
2021-08-17 14:13:48 +0100

    Add list of storage demos (placeholder for now)

--- ScopedStorage/app/src/main/java/com/samples/storage/scopedstorage/MainActivity.kt
+++ ScopedStorage/app/src/main/java/com/samples/storage/scopedstorage/MainActivity.kt
@@ -3,36 +3,90 @@ package com.samples.storage.scopedstorage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.ListItem
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.samples.storage.scopedstorage.ui.theme.ScopedStorageTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ScopedStorageTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = { Text(stringResource(R.string.app_name)) })
+                    },
+                    content = {
+                        NavHost(navController = navController, startDestination = HomeRoute) {
+                            composable(HomeRoute) {
+                                HomeScreen(navController)
+                            }
+
+                            composable(Demos.AddMediaFile.route) { NotAvailableYetScreen() }
+                            composable(Demos.CaptureMediaFile.route) { NotAvailableYetScreen() }
+                            composable(Demos.DownloadMediaFile.route) { NotAvailableYetScreen() }
+                            composable(Demos.AddFileToDownloads.route) { NotAvailableYetScreen() }
+                            composable(Demos.EditMediaFile.route) { NotAvailableYetScreen() }
+                            composable(Demos.DeleteMediaFile.route) { NotAvailableYetScreen() }
+                            composable(Demos.ListMediaFiles.route) { NotAvailableYetScreen() }
+                            composable(Demos.SelectDocumentFile.route) { NotAvailableYetScreen() }
+                            composable(Demos.CreateDocumentFile.route) { NotAvailableYetScreen() }
+                            composable(Demos.EditDocumentFile.route) { NotAvailableYetScreen() }
+                        }
+                    }
+                )
             }
         }
     }
 }
 
+const val HomeRoute = "home"
+
+@ExperimentalMaterialApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen(navController: NavController) {
+    LazyColumn {
+        items(Demos.list) { demo ->
+            ListItem(
+                modifier = Modifier.clickable { navController.navigate(demo.route) },
+                text = { Text(stringResource(demo.name)) },
+                secondaryText = { Text(stringResource(demo.description)) },
+                icon = { Icon(demo.icon, contentDescription = null) }
+            )
+            Divider()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ScopedStorageTheme {
-        Greeting("Android")
+fun NotAvailableYetScreen() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(stringResource(R.string.demo_not_available_yet_label))
     }
 }
