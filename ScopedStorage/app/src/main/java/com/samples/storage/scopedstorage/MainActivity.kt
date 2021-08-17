8269035d0c51197e742fb5c6d735767ea062aae7
Yacine Rezgui <rezgui.y@gmail.com>
2021-08-17 10:42:33 +0100

    Add files generated by Android Studio

---
+++ ScopedStorage/app/src/main/java/com/samples/storage/scopedstorage/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.samples.storage.scopedstorage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.samples.storage.scopedstorage.ui.theme.ScopedStorageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScopedStorageTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ScopedStorageTheme {
+        Greeting("Android")
+    }
+}
